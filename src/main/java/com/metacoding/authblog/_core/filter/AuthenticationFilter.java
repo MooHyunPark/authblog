299fd6333778b1701f822130c51805af92d8dac3
angus <angus1208@naver.com>
2024-11-27 17:57:06 +0900

    필터 구현 완료

--- src/main/java/com/metacoding/authblog/_core/filter/AuthenticationFilter.java
+++ src/main/java/com/metacoding/authblog/_core/filter/AuthenticationFilter.java
@@ -1,15 +1,28 @@
 package com.metacoding.authblog._core.filter;
 
+import com.metacoding.authblog.user.User;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.util.logging.LogRecord;
+
 
 public class AuthenticationFilter implements Filter {
 
-
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession();
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            resp.sendRedirect(req.getContextPath() + "/login-form");
+        } else {
+            chain.doFilter(request, response);
+        }
 
     }
 }
